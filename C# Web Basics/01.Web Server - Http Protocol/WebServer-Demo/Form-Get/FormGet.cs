421d98ebb82bfe28760238353d50915d3568cf87
emilia98 <emilia_nedialkova@abv.bg>
2019-06-05 17:46:12 +0300

    completed demos

---
+++ C# Web Basics/01.Web Server - Http Protocol/WebServer-Demo/Form-Get/FormGet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Form_Get
+{
+    class FormGet
+    {
+        static void Main()
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 3000);
+
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient client = tcpListener.AcceptTcpClient();
+
+                using (NetworkStream stream = client.GetStream())
+                {
+                    var requestBytes = new byte[100000];
+                    int readBytes = stream.Read(requestBytes, 0, requestBytes.Length);
+                    var stringRequest = Encoding.UTF8.GetString(requestBytes, 0, requestBytes.Length);
+
+                    Console.WriteLine(new string('-', 15));
+                    Console.WriteLine(stringRequest);
+
+                    string responseBody = "<form> <input type=\"text\" name=\"username\" /> <input type=\"password\" name=\"password\" /> <input type=\"submit\" /></form>";
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine("HTTP/1.0 200 OK");
+                    sb.AppendLine("Content-Type: text/html");
+                    sb.AppendLine($"Content-Length: ${responseBody.Length}");
+                    sb.AppendLine();
+                    sb.AppendLine(responseBody);
+
+                    string response = sb.ToString();
+                    var responseBytes = Encoding.UTF8.GetBytes(response);
+
+                    stream.Write(responseBytes, 0, responseBytes.Length);
+                }
+            }
+        }
+    }
+}
