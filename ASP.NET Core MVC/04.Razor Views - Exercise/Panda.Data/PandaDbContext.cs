a85e2fee4f7b315da25cefd719e504a9fbe9fd75
emilia98 <emilia_nedialkova@abv.bg>
2019-07-05 01:09:47 +0300

    added webs

---
+++ ASP.NET Core MVC/04.Razor Views - Exercise/Panda.Data/PandaDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Panda.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panda.Data
+{
+    public class PandaDbContext : IdentityDbContext<PandaUser>
+    {
+        // public DbSet<PandaUser> Users { get; set; }
+
+        public PandaDbContext(DbContextOptions<PandaDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PandaUser>().ToTable("User");
+        }
+    }
+}
---
+++ ASP.NET Core MVC/04.Razor Views - Exercise/Panda.Models/PandaUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panda.Models
+{
+    public class PandaUser : IdentityUser
+    {
+        public List<Package> Packages { get; set; }
+
+        public List<Receipt> Receipts { get; set; }
+
+        public PandaUser()
+        {
+            this.Packages = new List<Package>();
+            this.Receipts = new List<Receipt>();
+        }
+    }
+}
