891ce95db744f2bb0d465c386455969fe26e772d
emilia98 <emilia_nedialkova@abv.bg>
2019-07-26 00:25:34 +0300

    async demo (COMPLETED)

---
+++ C# Web Basics/03. Asynchronous Processing/Thread/ExceptionHandling/ExceptionHandling.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace ExceptionHandling
+{
+    class ExceptionHandling
+    {
+        static void Main()
+        {
+            /* Exceptions cannot be handled outside thread
+            try
+            {
+                new Thread(Wrong).Start();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            */
+
+            // Exceptions should be handled inside the executed methods
+            new Thread(Correct).Start();
+        }
+
+        static void Wrong()
+        {
+            throw new Exception("!!!!!!!");
+        }
+
+        static void Correct()
+        {
+            try
+            {
+                throw new Exception("*** exception ***");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception handled!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
