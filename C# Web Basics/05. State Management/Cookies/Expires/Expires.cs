408b724a72a9940138f0323663da4defd77a7940
emilia98 <emilia_nedialkova@abv.bg>
2019-07-27 19:46:28 +0300

    added demo (COMPLETED)

---
+++ C# Web Basics/05. State Management/Cookies/Expires/Expires.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expires
+{
+    class Expires
+    {
+        static async Task Main()
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 3000);
+
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient client = tcpListener.AcceptTcpClient();
+
+                await Task.Run(() => ProcessClient(client));
+            }
+        }
+
+        public static async Task ProcessClient(TcpClient client)
+        {
+            var NewLine = Environment.NewLine;
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                var requestBytes = new byte[100000];
+                int readBytes = await stream.ReadAsync(requestBytes, 0, requestBytes.Length);
+                var stringRequest = Encoding.UTF8.GetString(requestBytes, 0, readBytes);
+
+                Console.WriteLine(new string('-', 15));
+                Console.WriteLine(stringRequest);
+
+                string responseBody = stringRequest;
+
+                string response =
+                    "HTTP/1.0 200 OK" + NewLine +
+                    "Message: Hello!!!" + NewLine +
+                    "Content-Type: text/plain" + NewLine +
+                    "Set-Cookie: cookie1=test; Expires=" + DateTime.UtcNow.AddSeconds(30).ToString("R") + NewLine +
+                    // If we want to clear a cookie from the browser, set the time in the past
+                    /*"Set-Cookie: cookie1=test; Expires=" + DateTime.UtcNow.AddSeconds(-1).ToString("R") + NewLine +*/
+                    $"Content-Length: {responseBody.Length}" + NewLine + NewLine +
+                    responseBody;
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+                await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+            };
+        }
+    }
+}
---
+++ C# Web Basics/05. State Management/Cookies/Security/Security.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security
+{
+    class Security
+    {
+        static async Task Main()
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 3000);
+
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient client = tcpListener.AcceptTcpClient();
+
+                await Task.Run(() => ProcessClient(client));
+            }
+        }
+
+        public static async Task ProcessClient(TcpClient client)
+        {
+            var NewLine = Environment.NewLine;
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                var requestBytes = new byte[100000];
+                int readBytes = await stream.ReadAsync(requestBytes, 0, requestBytes.Length);
+                var stringRequest = Encoding.UTF8.GetString(requestBytes, 0, readBytes);
+
+                Console.WriteLine(new string('-', 15));
+                Console.WriteLine(stringRequest);
+
+                string responseBody = stringRequest;
+
+                string response =
+                    "HTTP/1.0 200 OK" + NewLine +
+                    "Message: Hello!!!" + NewLine +
+                    "Content-Type: text/plain" + NewLine +
+                    // document.cookie
+                    "Set-Cookie: cookie1=test; HttpOnly;" + NewLine +
+                    "Set-Cookie: cookie2=test; HttpOnly; Secure;" + NewLine +
+                    "Set-Cookie: cookie3=test; Secure;" + NewLine +
+                     "Set-Cookie: cookie4=test;" + NewLine +
+                    $"Content-Length: {responseBody.Length}" + NewLine + NewLine +
+                    responseBody;
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+                await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+            };
+        }
+    }
+}
